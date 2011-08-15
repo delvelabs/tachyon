a5f84cb8e5ad7c552952e3fde4fc6a198ca3563c
Gabriel Tremblay <initnull@gmail.com>
2011-08-14 22:18:27 -0400

    - Removed useless init module version

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -15,5 +15,4 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 #
-from core import conf
-Version = conf.version
+
--- plugins/path/__init__.py
+++ plugins/path/__init__.py
@@ -15,8 +15,6 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 #
-from core import conf
-Version = conf.version
 
 # Add all path plugins here
 __all__ = []
