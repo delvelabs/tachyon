f5b85bfcddae905dbb6f88623995f8544fd51239
Louis-Philippe Huberdeau <lp@huberdeau.info>
2018-07-31 09:48:23 -0400

    Upgrading to latest hammertime

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         ]
     },
     install_requires=[
-        'hammertime-http>=0.4.6,<0.5',
+        'hammertime-http>=0.5.0,<0.6',
         'easyinject==0.3',
         'click>=6.7,<7'
     ],
